363c23fa23b23a35672ccb191d1d72bcf8e48bff
imaiguo <imaiguo@gmail.com>
2025-09-16 16:06:13 +0800

    done

--- 02Advanced/src/components/WelcomeCatalog.vue
+++ 02Advanced/src/components/WelcomeCatalog.vue
@@ -5,4 +5,15 @@
 <template>
     <h2>目录</h2>
     <a href="html/NavigationGuards.html" target="_blank">1. 导航守卫</a><br/>
+    <a href="html/Meta.html" target="_blank">2. 路由元信息</a><br/>
+    <a href="html/DataFetching.html" target="_blank">3. 数据获取</a><br/>
+
+    <a href="html/RouterViewSlot.html" target="_blank">4. RouterView 插槽</a><br/>
+    <a href="html/Transitions.html" target="_blank">5. 过渡动效</a><br/>
+    <a href="html/ScrollBehavior.html" target="_blank">6. 滚动行为</a><br/>
+    <a href="html/LazyLoading.html" target="_blank">7. 路由懒加载</a><br/>
+    <a href="#" >8. 类型化路由 (v4.1.0+)</a><br/>
+    <a href="#">9. 扩展 RouterLink</a><br/>
+    <a href="html/NavigationFailures.html" target="_blank">10. 导航故障</a><br/>
+    <a href="html/DynamicRouting.html" target="_blank">11. 动态路由</a><br/>
 </template>
---
+++ 02Advanced/src/router/NavigationGuards.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
+})
+
+router.beforeEach((to, from) => {
+  console.log("Router beforeEach")
+  console.log(to)
+  console.log(from)
+  // 返回 false 以取消导航
+  return false
+})
+
+export default router
